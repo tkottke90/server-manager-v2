fb0317afb4af1a79786d215c6c031cb04ea1691d
Thomas Kottke <t.kottke90@gmail.com>
2020-02-03 20:06:29 -0500

    chore: Add development config

--- app/api/src/sequelize.config.js
+++ app/api/src/sequelize.config.js
@@ -1,14 +1,26 @@
 module.exports = {
+    development: {
+        "username": process.env.DATABASE_USER,
+        "password": process.env.DATABASE_PASSWORD,
+        "database": process.env.DATABASE_NAME,
+        "host": process.env.DATABASE_HOST,
+        "port": process.env.DATABASE_PORT,
+        "dialect": "postgres",
+        // "ssl": true,
+        // "dialectOptions": {
+        //     "ssl": true
+        // },
+    },
     production: {
         "username": process.env.DATABASE_USER,
         "password": process.env.DATABASE_PASSWORD,
         "database": process.env.DATABASE_NAME,
         "host": process.env.DATABASE_HOST,
         "port": process.env.DATABASE_PORT,
+        "dialect": "postgres",
         // "ssl": true,
         // "dialectOptions": {
         //     "ssl": true
         // },
-        "dialect": "postgres",
     },
 };
