9ffdcc9645d7f2ea4f75fef41aacab5e6a0de672
Thomas Kottke <t.kottke90@gmail.com>
2020-02-09 15:52:05 -0500

    refactor: Update user route to use auth hooks

--- app/api/src/routes/users.route.ts
+++ app/api/src/routes/users.route.ts
@@ -2,45 +2,46 @@ import DataModelRoute from '../classes/data-model.class';
 import Application from '../classes/application.class';
 
 import { IHooksArray, IModelHooks } from '../interfaces';
+import { hashFieldsHook } from '../hooks';
 
 class UsersRoute extends DataModelRoute {
 
   constructor(app: Application) {
     super(app, 'users', { exclusions: ['password'] });
 
-    this.configure('User', { before: beforeHooks, after: afterHooks, error: errorHooks } as IModelHooks);
+    this.configure('User', { before: this.beforeHooks, after: this.afterHooks, error: this.errorHooks } as IModelHooks);
   }
 
-}
+  private beforeHooks: IHooksArray = {
+    all: [ this.app.authentication.jwtAuth ],
+    find: [],
+    get: [],
+    create: [ hashFieldsHook(['password']) ],
+    update: [ hashFieldsHook(['password']) ],
+    updateOrCreate: [ hashFieldsHook(['password']) ],
+    delete: []
+  }
 
-const beforeHooks: IHooksArray = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  updateOrCreate: [],
-  delete: []
-}
+  private afterHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
 
-const afterHooks: IHooksArray = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  updateOrCreate: [],
-  delete: []
-}
+  private errorHooks: IHooksArray = {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    updateOrCreate: [],
+    delete: []
+  }
 
-const errorHooks: IHooksArray = {
-  all: [],
-  find: [],
-  get: [],
-  create: [],
-  update: [],
-  updateOrCreate: [],
-  delete: []
 }
 
 export function initialize(app: Application) {
