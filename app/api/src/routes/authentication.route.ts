70754a5da34b1755c3034ac2dade5779689bc90b
Thomas Kottke <t.kottke90@gmail.com>
2020-02-09 21:36:28 -0500

    refactor: Update refresh token to use IContext interface

--- app/api/src/routes/authentication.route.ts
+++ app/api/src/routes/authentication.route.ts
@@ -11,15 +11,23 @@ class AuthenticationRoute extends BaseRoute {
     this.setup({
         routes: [
           { method: 'post', path: '/', action: app.authentication.localAuth },
-          { method: 'post', path: '/refresh', action: (context: IContext) => this.refreshToken },
+          { method: 'post', path: '/refresh', action: this.refreshToken },
         ]
     });
   }
 
-  public refreshToken = async (request: Request, response: Response) => {
-    const token = request.headers.authorization.split(' ')[1];
-    const result = await this.app.authentication.refreshToken(token);
-    response.json({ token: result });
+  public refreshToken = (context: IContext) => {
+    return new Promise( async (resolve, reject) => {
+      const token = context.request.headers.authorization.split(' ')[1];
+      try {
+        const result = await this.app.authentication.refreshToken(token);
+        resolve(result);
+      } catch (err) {
+        context.app.logger.error(err);
+        reject(err);
+      }
+      
+    });
   }
 }
 
