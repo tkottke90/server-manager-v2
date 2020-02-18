8eb0080289bea66142eccb8549ba6a41cbdaebe8
Thomas Kottke <t.kottke90@gmail.com>
2020-02-17 23:56:49 -0500

    refactor: Add materialdesignicons

--- app/ui/src/app/app.module.ts
+++ app/ui/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BaseComponent } from './components';
+
+import { MatIconRegistry, MatIconModule } from '@angular/material/icon'
+import { MatToolbar } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +20,17 @@ import { BaseComponent } from './components';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpModule,
+    // Angular Material
+    MatIconModule,
+    MatToolbar
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg'));
+  }
+}
