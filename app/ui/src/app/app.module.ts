38eeeb0abb7e5f11b6870bbe3479a08617c8ce67
Thomas Kottke <t.kottke90@gmail.com>
2020-02-18 20:43:43 -0500

    refactor: fix http module import

--- app/ui/src/app/app.module.ts
+++ app/ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { HttpModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import { MatToolbar } from '@angular/material/toolbar';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpModule,
+    HttpClientModule,
     // Angular Material
     MatIconModule,
     MatToolbar
