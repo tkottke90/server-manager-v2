fda4de7c48f106dd427d07621b17ea1155c80b2f
Thomas Kottke <t.kottke90@gmail.com>
2020-02-22 09:22:08 -0500

    feat: Add 404 page

--- app/ui/src/app/app.module.ts
+++ app/ui/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
