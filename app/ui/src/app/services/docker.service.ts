54446c55c45dd30a14ce9397d5847efed1204caa
Thomas Kottke <t.kottke90@gmail.com>
2020-03-13 17:53:47 -0400

    feat(ui): Add docker service actions

--- app/ui/src/app/services/docker.service.ts
+++ app/ui/src/app/services/docker.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@angular/core';
 import { SocketService } from './socket.service';
-
-
+import { Observable, fromEvent } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DockerService {
 
+  getContainerEvent: Observable<any[]>;
+
   constructor(
     private socketService: SocketService
   ) {
-    socketService.emit({ event: 'get containers', data: []})
+    this.init();
   }
 
+  init() {
+    this.getContainerEvent = this.socketService.listen('get containers');
+  }
+
+  getContainers() {
+    this.socketService.emit({ event: 'get containers', data: []});
+  }
   
+  getContainer(name: String) {
+    this.socketService.emit({ event: 'get container', data: [{ name }]})
+  }
 }
