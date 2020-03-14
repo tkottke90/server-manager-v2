cf174bf132bd5491978a32d1c4f2f063fc1fa6c2
Thomas Kottke <t.kottke90@gmail.com>
2020-03-13 19:46:36 -0500

    refactor: Add socket status to socket service

--- app/ui/src/app/services/socket.service.ts
+++ app/ui/src/app/services/socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
-import { fromEvent } from 'rxjs';
+import { fromEvent, BehaviorSubject } from 'rxjs';
 
 export interface ISocketEvent {
   event: string;
@@ -15,15 +15,37 @@ export class SocketService {
 
   private socket: SocketIOClient.Socket;
 
+  public socketStatus: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   constructor() {
+    console.log('new socket service');
     this.socket = io({
       transports: ['websocket'],
       secure: true,
       upgrade: false
     });
+
+    fromEvent(this.socket, 'connect')
+      .subscribe( event => {
+        this.socketStatus.next(true);
+      });
+
+    fromEvent(this.socket, 'disconnect')
+      .subscribe( event => {
+        this.socketStatus.next(false);
+      });
+
+    fromEvent(this.socket, 'reconnect')
+      .subscribe( event => {
+        this.socketStatus.next(true);
+      });
   }
 
-  
+  disconnect() {
+    this.socket.disconnect();
+    this.socket = null;
+  }
+
   listen(eventName: string) {
     // this.socket.on(eventName, action);
     return fromEvent<any[]>(this.socket, eventName);
